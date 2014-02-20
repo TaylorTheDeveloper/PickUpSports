c0f02516e47ce7dcec9827ce277baf27086d9c36
Taylor <TaylorTheDeveloper@gmail.com>
2014-02-20 01:06:39 -0500

    Cleaned up insert, fixed broken values.
    Now storing game type as string, easier to dynamically add game types

--- insert.php
+++ insert.php
@@ -1,58 +1,35 @@
-	<?php //Print Database
- 			/* Keep this for now
- 			$p_location = $_POST["location"];
- 			$p_sport = $_POST["sports"];
- 			$p_date= $_POST["date"];
- 			$p_time= $_POST["time"];
- 			$p_currPlayers = $_POST["currPlayers"];
- 			$p_needPlayers = $_POST["maxPlayers"];
- 			$radio_value = $_POST["pubpriv"];
- 			*/
- 			/*if(isset($_POST['pubpriv']))
-		   {
-		    //other variables
-		     $radio_value = $_POST["pubpriv"];
-		   }
-		   else
-		   {
-		   	echo "not set";
-		   }*/
+<?php 
+	$p_pubPriv = 0;
+	if($_POST["pubPriv"]=="public"){
+		$p_pubPriv =1;
+	}
 
- 			$p_location = $_POST["location"];
- 			$p_sport = $_POST["sports"]; //Need to decode into number, or modify database. Devs need to decide
- 			$p_date= $_POST["date"];
- 			$p_time= $_POST["time"];
- 			$p_zip= $_POST["zip"];
- 			//$p_currPlayers = $_POST["currPlayers"];
- 			$p_maxPlayers = $_POST["maxPlayers"];
- 			//$p_pubPriv = $_POST["pubPriv"];
+	//Connect
+	$con=mysqli_connect("localhost:3306","root","","games");
+	// Check connection
+	if (mysqli_connect_errno())
+	  {
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	  }
 
-			//Connect
-			$con=mysqli_connect("localhost:3306","root","","games");
-			// Check connection
-			if (mysqli_connect_errno())
-			  {
-			  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-			  }
+	 $sql="INSERT INTO matches (`match_id`, `admin_user_id`, 
+	 	`match_type`, `match_location`, 
+	 	`match_zip`, `match_date`, 
+	 	`match_time`, `match_maxplayers`, 
+	 	`match_currentplayers`, `matchp_pubpriv`) 
+		VALUES ( 0, 0 , 
+			'$_POST[sports]', '$_POST[location]', 
+		 	'$_POST[zip]', '$_POST[date]', 
+		 	'$_POST[time]','$_POST[maxPlayers]', 
+		 	1, '$p_pubPriv')"; //Only one player, Match id is auto gen, user id can wait
+	// Test code : //$ssqqll="INSERT INTO `matches`(`match_id`, `admin_user_id`, `match_type`, `match_location`, `match_zip`, `match_date`, `match_time`, `match_maxplayers`, `match_currentplayers`, `matchp_pubpriv`) VALUES (0,0,0,0,0,0,0,0,0,0)";
+	
+	if (!mysqli_query($con,$sql))
+	  {
+	  die('Error: ' . mysqli_error($con));
+	  }
+	echo "1 record added"; //From here, we need to go to an edit page. 
 
-			 $sql="INSERT INTO matches (`match_id`, `admin_user_id`, 
-			 	`match_type`, `match_location`, 
-			 	`match_zip`, `match_date`, 
-			 	`match_time`, `match_maxplayers`, 
-			 	`match_currentplayers`, `matchp_pubpriv`) 
-				VALUES ( 0, 0 ,
-					1, '$_POST[location]', 
-				 	'$_POST[zip]', '$_POST[date]', 
-				 	'$_POST[time]','$_POST[maxPlayers]', 
-				 	1, 0)"; //Only one player
-			$ssqqll="INSERT INTO `matches`(`match_id`, `admin_user_id`, `match_type`, `match_location`, `match_zip`, `match_date`, `match_time`, `match_maxplayers`, `match_currentplayers`, `matchp_pubpriv`) VALUES (0,0,0,0,0,0,0,0,0,0)";
-			
-			if (!mysqli_query($con,$ssqqll))
-			  {
-			  die('Error: ' . mysqli_error($con));
-			  }
-			echo "1 record added";
-
-			mysqli_close($con);
-			?>
-				'$_POST[firstname]'
+	mysqli_close($con);
+?>
+				
