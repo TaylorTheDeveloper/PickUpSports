7f3af84b2e7150ccb4fbfaf0460755e2be9040f1
Taylor <TaylorTheDeveloper@gmail.com>
2014-02-22 10:55:57 -0800

    added fonts

--- DATABASE_FILES/README.txt
+++ DATABASE_FILES/README.txt
@@ -4,6 +4,6 @@ Login:
 user='root'
 pass='' //blank
 
-go to import, select this games.sql file
+go to import, select this games_newest.sql file
 
-This is just the database structure, it has no contents. 
+CHECK FOR THE NEWEST DATE BEFORE TO MAKE SURE YOU HAVE THE RIGHT DATABASE!!!!
--- userCakeV2.0.2/models/db-settings.php
+++ userCakeV2.0.2/models/db-settings.php
@@ -5,10 +5,10 @@ http://usercake.com
 */
 
 //Database Information
-$db_host = "localhost"; //Host address (most likely localhost)
-$db_name = "202"; //Name of Database
-$db_user = "202"; //Name of database user
-$db_pass = "password"; //Password for database user
+$db_host = "localhost:3306"; //Host address (most likely localhost)
+$db_name = "games"; //Name of Database
+$db_user = "root"; //Name of database user
+$db_pass = ""; //Password for database user
 $db_table_prefix = "uc_";
 
 GLOBAL $errors;
