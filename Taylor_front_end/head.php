a1e867062f013b2ee0cf6145af770df259ca908e
Taylor <TaylorTheDeveloper@gmail.com>
2014-02-25 20:08:37 -0800

    Rob, here's what I threw together in class earlier
    Front end stuff

---
+++ Taylor_front_end/head.php
@@ -0,0 +1,18 @@
+<head>
+    <title>PickUp Sports</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap StyleSheets -->
+  <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+  <link rel="stylesheet" href="css/style.css" type="text/css" />
+  <link rel="stylesheet" href="css/simple-sidebar.css" />
+ 
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+     <script src="https://code.jquery.com/jquery.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+      <script src="js/slider.js"></script>
+  </head>
---
+++ Taylor_front_end/navbar.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+        <!-- Logo Here! -->
+    <a class="navbar-brand" href="index.php">PickUp Sports <!-- <img id="logo" src="images/drinkr_logo_smaller_alt_2.png" /> --> </a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <!-- <ul class="nav navbar-nav navbar-right"> 
+      <li><a href="#">What's PickUp?</a></li>
+      <li><a href="#">Temp</a></li>
+    </ul> -->
+    <form class="navbar-form navbar-right" role="search" method="post" action="index.php">
+      <div class="form-group">
+        <input type="text" name="username" class="form-control" placeholder="Username">
+        <input type="text" name="password" class="form-control" placeholder="Password">
+      </div>
+      <button type="submit" class="btn btn-primary btn-sm">Login</button>
+    </form>   
+  </div><!-- /.navbar-collapse -->
+</nav>
