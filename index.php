9e850a15b42e75d58638cfef255da3479918f3f3
Taylor <TaylorTheDeveloper@gmail.com>
2014-03-01 01:08:44 -0500

    added some pictures

--- index.php
+++ index.php
@@ -12,17 +12,23 @@
 <!-- Doc Slider -->
 <?php include 'slider.html' ?>
 
-
-<div class="container">
+<br><br><br>
+<div class="container" >
 
     <div class="row clearfix">
             <div class="col-md-3 column">
             </div>
             <div class="col-md-3 column">
             <form class="form-signin mg-btm" method="post" action="newgame.php">
+            <center>
+            <img class="img-circle img-responsive text-center" src="http://lorempixel.com/200/200/sports/8">
+                <br>
                 <h3 class="heading-desc pull-center">Create A Game</h3>
+                <br>
                 <div class="main">          
-                 <label for="Sports">Sport</label> <select name="sports">
+                 <label for="Sports">Sport</label>
+                 <br>
+                 <select name="sports">
                                                 <option style="color:gray" value="Choose a sport" selected="selected" > </option>
                                                 <option value="Baseball">Baseball</option>
                                                 <option value="Football">Football</option>
@@ -36,23 +42,28 @@
                                             </select>
                 <span class="clearfix"></span>  
                 </div>
+                <br>
                 <div class="login-footer">
                 <div class="row">
-                                <div class="col-xs-6 col-md-6 pull-center">
                                 <input class="btn btn-info" type="submit" value="Let's Play!"> 
                                    <!--  <a href="newgame.html" class="btn btn-success " ><i class="icon-star icon-white "></i> Lets Play!</a> -->
                                 </div>
-                            </div>
-                
                 </div>
+                </center>
             </form>
             </div>
             <div class="col-md-3 column">
             <!-- Existing Game -->
                 <form class="form-signin mg-btm" method="post" action="search.php">
+                <center>
+                <img class="img-circle img-responsive text-center" src="http://lorempixel.com/200/200/sports/7">
+                    <br>
                     <h3 class="heading-desc pull-center">Find A Match</h3>
+                    <br>
                     <div class="main">          
-                     <label for="Sports">Sport</label> <select name="sports">
+                     <label for="Sports">Sport</label> 
+                     <br>
+                     <select name="sports">
                                                     <option style="color:gray" value="Choose a sport" selected="selected" > </option>
                                                     <option value="Baseball">Baseball</option>
                                                     <option value="Football">Football</option>
@@ -67,15 +78,14 @@
                     <label for="zip">Zip</label><input type="text" class="form-control" id="zip_location" name="zip_location" /><br>
                     <span class="clearfix"></span>  
                     </div>
+                    <br>
                     <div class="login-footer">
                     <div class="row">
-                                    <div class="col-xs-6 col-md-6 pull-center">
                                     <input class="btn btn-info" type="submit" value="Search"> 
                                         <!-- <a href="search.php" class="btn btn-success" ><i class="icon-star icon-white "></i> Search</a> -->
-                                    </div>
-                                </div>
-                    
+                                    </div>                    
                     </div>
+                    </center>
                 </form>
             </div>
             <div class="col-md-3 column">
