a2bef73d792f8cc174de88e20baccbcd2d1272fd
Taylor <TaylorTheDeveloper@gmail.com>
2014-03-19 01:52:22 -0400

    CHECK OUT DAT USERPAGE!

---
+++ DATABASE_FILES/games_3-19-18.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 19, 2014 at 05:51 AM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `games`
+--
+CREATE DATABASE IF NOT EXISTS `games` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `games`;
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `newMatch`(IN `userID` INT(5), IN `sport` INT(10), IN `location` VARCHAR(256), IN `zip` INT(5), IN `date` DATE, IN `time` TIME, IN `maxPlayers` INT(5), IN `currPlayers` INT(5), IN `pubpriv` BOOLEAN)
+    NO SQL
+INSERT INTO matches (    
+	admin_user_id,     
+	match_type, 
+	match_location, 
+	match_zip,       
+	match_date,     
+	match_time,            
+	match_maxplayers,      
+	match_currentplayers,  
+	matchp_pubpriv
+	)
+  VALUES (
+    @userID,     
+	@sport, 
+	@location, 
+	@zip,
+	@date,       
+	@time,     
+	@maxPlayers,            
+	@currPlayers,      
+	@pubpriv 
+  )$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `testSearch`()
+BEGIN
+    SELECT * FROM matches WHERE match_zip=32304;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gamehistory`
+--
+
+CREATE TABLE IF NOT EXISTS `gamehistory` (
+  `username` varchar(256) NOT NULL,
+  `user_idnum` int(11) NOT NULL,
+  `baseball` int(11) NOT NULL,
+  `soccer` int(11) NOT NULL,
+  `football` int(11) NOT NULL,
+  `tennis` int(11) NOT NULL,
+  `frisbee` int(11) NOT NULL,
+  `rugby` int(11) NOT NULL,
+  `basketball` int(11) NOT NULL,
+  `hockey` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `gamehistory`
+--
+
+INSERT INTO `gamehistory` (`username`, `user_idnum`, `baseball`, `soccer`, `football`, `tennis`, `frisbee`, `rugby`, `basketball`, `hockey`) VALUES
+('', 1, 20, 27, 20, 25, 26, 28, 19, 5),
+('taylor', 1, 5, 6, 7, 0, 8, 9, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matches`
+--
+
+CREATE TABLE IF NOT EXISTS `matches` (
+  `match_id` int(5) NOT NULL AUTO_INCREMENT COMMENT 'auto-value',
+  `admin_user_id` int(5) NOT NULL COMMENT 'user id number',
+  `match_type` varchar(18) NOT NULL COMMENT 'value',
+  `match_location` varchar(256) NOT NULL COMMENT 'Where its at',
+  `match_zip` int(5) NOT NULL COMMENT '5 digit zip',
+  `match_date` date NOT NULL COMMENT 'day of match',
+  `match_time` time NOT NULL COMMENT 'time, 24 hour clock',
+  `match_maxplayers` int(5) NOT NULL,
+  `match_currentplayers` int(5) NOT NULL,
+  `matchp_pubpriv` tinyint(1) NOT NULL COMMENT 'zero for public, 1 for private',
+  `playerlist` varchar(1000) NOT NULL COMMENT 'List of players. usernames separated by ;',
+  PRIMARY KEY (`match_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='matches_prototype' AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `matches`
+--
+
+INSERT INTO `matches` (`match_id`, `admin_user_id`, `match_type`, `match_location`, `match_zip`, `match_date`, `match_time`, `match_maxplayers`, `match_currentplayers`, `matchp_pubpriv`, `playerlist`) VALUES
+(1, 4, 'Tennis', '608 west lafayette', 32304, '2014-02-26', '00:15:00', 16, 0, 0, ''),
+(2, 4, 'Soccer', 'The Gym at FsU', 32304, '2014-02-24', '00:17:30', 8, 2, 0, 'Joe; Karl; Tyler'),
+(3, 4, 'Basketball', 'Wescott Fountain', 32308, '2014-02-28', '00:13:00', 10, 0, 0, 'Lawl; LAMO; KoOl; Eddie'),
+(4, 4, 'Hockey', 'FAMU Courts', 32301, '2014-02-23', '00:17:30', 4, 2, 1, 'Front; back; left; right'),
+(5, 5, 'Baseball', 'bobs', 32304, '0000-00-00', '23:50:26', 4, 6, 1, 'Jimmy; Joel; Frank'),
+(6, 0, 'Baseball', 'Tallahassee', 32304, '2014-02-28', '05:30:00', 4, 1, 1, 'This; Never; Ends'),
+(7, 0, 'Football', 'aaa', 32304, '2014-02-06', '08:15:00', 4, 1, 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_idnum` int(11) NOT NULL AUTO_INCREMENT COMMENT 'user_id',
+  `first_name` varchar(30) NOT NULL COMMENT 'f_name',
+  `last_name` varchar(40) NOT NULL COMMENT 'l_name',
+  `email` varchar(60) NOT NULL COMMENT 'email',
+  `username` varchar(32) NOT NULL COMMENT 'username',
+  `zip` int(11) NOT NULL COMMENT 'zip',
+  `password` varchar(32) NOT NULL COMMENT 'max length is 32',
+  `favSport` varchar(20) NOT NULL,
+  `gamesPlayed` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_idnum`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_idnum`, `first_name`, `last_name`, `email`, `username`, `zip`, `password`, `favSport`, `gamesPlayed`) VALUES
+(12, 'Rob', 'Shnayder', 'fef@lol.com', 'rob', 32304, 'coo', 'Hockey', 0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- userpage.php
+++ userpage.php
@@ -27,39 +27,63 @@
 				<div class="col-md-8 column">
 					<div class="media well">
 						 <?php //Query Database
-							$uid = $_SESSION['user_idnum'];
+						 //Handle Sports Value
+						 function random_color_part() {
+						    return str_pad( dechex( mt_rand( 150, 255 ) ), 3, '0', STR_PAD_LEFT);
+						}
+
+						function random_color() {
+						    return random_color_part();
+						    /* return random_color_part() . random_color_part() . random_color_part()*/
+						}
+						 function handleSports($Sval,$name,$max){
+						 	$percent = ($Sval/$max) * 100;
+							if($Sval > 0){
+								echo "<div style=\"margin: 1%; width: 99%; border: 1px solid #07ad79;\">
+								<div style=\"color: #ffffff; padding: 5px; width:" . $percent . "%; background: #" . random_color() . ";\">" . $name . " " . $Sval . "</div>
+								</div>";
+								}
+							}
+
+							$uid = $_SESSION['username'];
 							//Connect
 							$con=mysqli_connect("localhost:3306","root","","games");
 							// Check connection
 							if (mysqli_connect_errno())
-							  {
+							{
 							  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-							  }
+							}
 
 							//Select Data
-							$result = mysqli_query($con,"SELECT * FROM gameHistory WHERE user_idnum = '$uid'" );
+							$result = mysqli_query($con,"SELECT * FROM gameHistory WHERE username = '$uid'" );
 							//Check that it returns true
 							if($result==false){
 							  echo "Master Wayne, I've failed you";
 							}
 
-							if(!(mysqli_fetch_array($result))){
-							    echo "Error Opening user Stats!";
-							}
-							$num = 1;
-							//Print Data by row
+							// if(!(mysqli_fetch_array($result))){
+							//     echo "Error Opening user Stats!";
+							// }
+							
 							while($row = mysqli_fetch_array($result))
 							  {
-							    echo $baseball = $row['baseball'];
-							    
-							  //       $playerlist = $row['playerlist'];        
-							        // $list = explode(";", $playerlist);
-							        // foreach ($list as $k=>$n){ 
-							        //     echo "$n<br>";
-							        // }    
-
-							        $num++;
+							  	$baseball = $row['baseball'];
+							    $basketball = $row['basketball'];
+							    $football = $row['football'];
+							    $soccer = $row['soccer'];
+							    $hockey = $row['hockey'];
+							    $tennis = $row['tennis'];
+							    $frisbee = $row['frisbee'];							 
 							  }
+							  $highVal = max($baseball, $basketball, $football, $soccer, $hockey, $tennis, $frisbee);
+
+							  handleSports($baseball, "Baseball", $highVal);
+							  handleSports($basketball,"Basketball", $highVal);
+							  handleSports($football,"Football", $highVal);
+							  handleSports($soccer,"Soccer", $highVal);
+							  handleSports($hockey,"Hockey", $highVal);
+							  handleSports($tennis,"Tennis", $highVal);
+							  handleSports($frisbee,"Frisbee", $highVal);
 
 
 
